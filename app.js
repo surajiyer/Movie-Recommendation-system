79db9e933ad5152470c52070fa37375b497eaec0
Suraj Iyer <suraj.iyer96@outlook.com>
2016-03-21 10:18:48 +0100

    Fixing cross-domain requests problem
    - Enabled CORS on response headers.

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(useragent.express());
 
 // Enable CORS
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Make our db accessible to our routes
 app.use(function(req,res,next){
